cbdf24ed244fed3cfefe33e32446d3900f4ac0b1
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-06 12:07:40 -0300

    format

---
+++ libs/eslint-config/eslint.config.d.ts
@@ -0,0 +1,9 @@
+declare module '@scope/eslint-config/node' {
+  const config: import('eslint').FlatConfig[];
+  export default config;
+}
+
+declare module '@scope/eslint-config/react' {
+  const config: import('eslint').FlatConfig[];
+  export default config;
+}
