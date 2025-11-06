47d0be7d24020912276d8ec20bd5691d5b6bb100
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-06 18:44:39 -0300

    format

--- libs/eslint-config/eslint.config.d.ts
+++ libs/eslint-config/eslint.config.d.ts
@@ -1,3 +1,5 @@
+// note: This file prevents the consumer from seeing "Could not find a declaration file for module"
+
 import { Config } from 'eslint/config';
 
 declare module '@scope/eslint-config/node' {
