43acbc249744f0e84b6959605cc6157dda3a0ab2
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-07 13:03:45 -0300

    format

--- libs/eslint-config/eslint.config.node.js
+++ libs/eslint-config/eslint.config.node.js
@@ -1,10 +1,11 @@
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import { eslintBaseConfig, sharedLanguageOptions } from './eslint.config.js';
 
-export default [
+export default defineConfig(
   // Extended configs
   eslintBaseConfig,
 
   // Settings
   { languageOptions: { ...sharedLanguageOptions, globals: globals.node } },
-];
+);
