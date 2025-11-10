278b26154eec12cd5fe05c67abacb053a39bd7bc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-10 12:06:34 -0300

    frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,4 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Allows access outside the docker container
-    watch: {
-      usePolling: true, // Enables watch mode inside the docker container
-    },
-  },
 });
