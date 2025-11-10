d12be4bb686bda587ca1c507c7a7b0146eb58ff9
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-10 17:58:49 -0300

    env

---
+++ services/media/src/config/env.ts
@@ -0,0 +1,39 @@
+import { NODE_ENVIRONMENTS } from '@/constants/node-environments';
+
+// ---------------------------
+// VALUES
+// ---------------------------
+
+const NODE_ENV = process.env.NODE_ENV;
+export const isTest = NODE_ENV === NODE_ENVIRONMENTS.TEST;
+export const isProduction = NODE_ENV === NODE_ENVIRONMENTS.PRODUCTION;
+export const isDevelopment = !isProduction && !isTest;
+
+// ---------------------------
+// GUARDS
+// ---------------------------
+
+if (!isProduction) {
+  // Disabled eslint: use "require()" to load dotenv synchronously
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  (require('dotenv') as typeof import('dotenv')).config({
+    path: '.env.dev',
+    quiet: isTest,
+  });
+}
+
+// ---------------------------
+// UTILS
+// ---------------------------
+
+const getRequiredEnv = (key: string) => {
+  const value = process.env[key];
+  if (!value) throw new Error(`Environment variable ${key} is required`);
+  return value;
+};
+
+// ---------------------------
+// ENV VARIABLES
+// ---------------------------
+
+export const PORT = Number(getRequiredEnv('PORT'));
