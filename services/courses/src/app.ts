fa296c4450b208340baae145e1adb6a8389177f9
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-13 11:50:31 -0300

    format

--- services/courses/src/app.ts
+++ services/courses/src/app.ts
@@ -1,7 +1,22 @@
 import express from 'express';
+import { EXCHANGES, ROUTING_KEYS } from './libs/rabbitmq/constants';
+import { publishEvent } from './libs/rabbitmq/utils/publish-event';
 
 export const app = express();
 
-app.use('/', (_, res) => {
+app.get('/', (_, res) => {
   res.json({ message: 'Hello courses' });
+});
+
+app.get('/rabbit', (_, res) => {
+  const publishedEvent = publishEvent(
+    EXCHANGES.COURSES,
+    ROUTING_KEYS.LECTURE_CREATED,
+    {
+      message: 'Test rabbitmq',
+    },
+  );
+  if (!publishedEvent) throw new Error('Message not published');
+
+  res.status(201).send('courses.created published');
 });
