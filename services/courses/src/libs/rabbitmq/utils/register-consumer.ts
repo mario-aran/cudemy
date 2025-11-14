c311565894000e838511b8e10f75f5c10e85dcf0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-14 08:37:13 -0300

    format

--- services/courses/src/libs/rabbitmq/utils/register-consumer.ts
+++ services/courses/src/libs/rabbitmq/utils/register-consumer.ts
@@ -1,17 +1,32 @@
 import { getChannel } from '@/libs/rabbitmq/connection';
+import { ConsumeMessage } from 'amqplib';
 
-export const registerConsumer = async (
-  queue: string,
-  messageHandler: (payload: unknown) => Promise<void> | void,
-) => {
+interface RegisterConsumerProps {
+  queue: string;
+  handler: (payload: unknown) => Promise<void> | void;
+  prefetch?: number;
+}
+
+export const registerConsumer = async ({
+  queue,
+  handler,
+  prefetch = 1,
+}: RegisterConsumerProps) => {
   const channel = getChannel();
-  await channel.consume(queue, (msg) => {
+  await channel.prefetch(prefetch);
+
+  const onMessage = (msg: ConsumeMessage | null) => {
     if (!msg) return;
 
     void (async () => {
-      const rawPayload = msg.content.toString();
-      await messageHandler(JSON.parse(rawPayload));
-      channel.ack(msg);
+      try {
+        const rawPayload = msg.content.toString();
+        await handler(JSON.parse(rawPayload));
+        channel.ack(msg);
+      } catch {
+        channel.nack(msg, false, false);
+      }
     })();
-  });
+  };
+  await channel.consume(queue, onMessage, { noAck: false });
 };
