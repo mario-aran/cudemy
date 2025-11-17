d1ab01d60251e2db5ec77e0d7114752a3339a940
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-17 11:32:09 -0300

    format

--- services/courses/src/index.ts
+++ services/courses/src/index.ts
@@ -1,12 +1,34 @@
 import { app } from './app';
 import { PORT } from './config/env';
 import { logger } from './libs/logger/winston';
-import { initRabbitMQ } from './libs/rabbitmq/connection';
+import { closeRabbitMQ, initRabbitMQ } from './libs/rabbitmq/connection';
+
+// ---------------------------
+// STARTUP
+// ---------------------------
 
 void (async () => {
   await initRabbitMQ();
 
   app.listen(PORT, () => {
-    logger.info(`Application started successfully on port ${String(PORT)}`);
+    logger.info('Application started successfully on port:', PORT);
   });
 })();
+
+// ---------------------------
+// SHUTDOWN
+// ---------------------------
+
+const gracefulShutdown = async () => {
+  console.log('shutdown:start');
+
+  // Close resources
+  await closeRabbitMQ();
+
+  // Process exit
+  console.log('shutdown:complete');
+  process.exit(0);
+};
+
+process.on('SIGINT', () => void gracefulShutdown());
+process.on('SIGTERM', () => void gracefulShutdown());
