b2534b0581cbcee12d3293680bdfc086600ba5f4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-17 16:31:43 -0300

    index

--- services/courses/src/index.ts
+++ services/courses/src/index.ts
@@ -1,17 +1,29 @@
 import { app } from './app';
 import { PORT } from './config/env';
 import { logger } from './libs/logger/winston';
-import { closeRabbitMQ, initRabbitMQ } from './libs/rabbitmq/connection';
+import { initRabbitMQ, shutdownRabbitMQ } from './libs/rabbitmq/connection';
 
 // ---------------------------
 // STARTUP
 // ---------------------------
 
 void (async () => {
-  await initRabbitMQ();
+  // Start resources
+  try {
+    await initRabbitMQ();
+  } catch (err) {
+    logger.error('startup:failed', err);
+    process.exit(1);
+  }
 
-  app.listen(PORT, () => {
-    logger.info('Application started successfully on port', PORT);
+  // Start server
+  const server = app.listen(PORT, () =>
+    logger.info('Application started successfully on port', PORT),
+  );
+
+  server.on('error', (err) => {
+    logger.error('startup:failed', err);
+    process.exit(1);
   });
 })();
 
@@ -19,16 +31,26 @@ void (async () => {
 // SHUTDOWN
 // ---------------------------
 
-const gracefulShutdown = async () => {
-  console.log('shutdown:start');
+const gracefulShutdown = () => {
+  void (async () => {
+    logger.info('shutdown:start');
 
-  // Close resources
-  await closeRabbitMQ();
+    // Shutdown resources
+    const resources = [shutdownRabbitMQ];
 
-  // Process exit
-  console.log('shutdown:complete');
-  process.exit(0);
+    for (const shutdownResource of resources) {
+      try {
+        await shutdownResource();
+      } catch (err) {
+        logger.error('shutdown:error', err);
+      }
+    }
+
+    // Process exit
+    logger.info('shutdown:complete');
+    process.exit(0);
+  })();
 };
 
-process.on('SIGINT', () => void gracefulShutdown());
-process.on('SIGTERM', () => void gracefulShutdown());
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
