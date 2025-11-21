90db735b9f671b23a2ba5503c7fe8632764a91b5
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-21 14:22:34 -0300

    format

--- services/media/src/index.ts
+++ services/media/src/index.ts
@@ -2,8 +2,47 @@ import { app } from './app';
 import { PORT } from './config/env';
 import { logger } from './libs/logger/winston';
 
+// ---------------------------
+// STARTUP
+// ---------------------------
+
 (() => {
-  app.listen(PORT, () => {
-    logger.info(`Application started successfully on port ${String(PORT)}`);
+  // try {
+  // } catch (err) {
+  //   logger.error('startup:failed', err);
+  //   process.exit(1);
+  // }
+
+  const server = app.listen(PORT, () =>
+    logger.info('startup:success on port', PORT),
+  );
+  server.on('error', (err) => {
+    logger.error('startup:failed', err);
+    process.exit(1);
   });
 })();
+
+// ---------------------------
+// SHUTDOWN
+// ---------------------------
+
+// const gracefulShutdown = () =>
+//   void (async () => {
+//     logger.info('shutdown:start');
+
+//     // Shutdown resources
+//     const shutdownFunctions = [];
+//     for (const shutdown of shutdownFunctions) {
+//       try {
+//         await shutdown();
+//       } catch (err) {
+//         logger.error('shutdown:error', err);
+//       }
+//     }
+
+//     // Process exit
+//     logger.info('shutdown:complete');
+//     process.exit(0);
+//   })();
+// process.on('SIGINT', gracefulShutdown);
+// process.on('SIGTERM', gracefulShutdown);
