ed46a3c6af2bc79ad1cff39d6001e2145d0f377b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-22 15:59:28 -0300

    format

--- packages/logger/src/morgan.ts
+++ packages/logger/src/morgan.ts
@@ -3,7 +3,7 @@
 import morgan from 'morgan';
 import winston from 'winston';
 
-export const createMorganInit = (logger: winston.Logger) =>
+export const createMorganMiddleware = (logger: winston.Logger) =>
   morgan('combined', {
     stream: { write: (message) => logger.http(message.trim()) },
   });
