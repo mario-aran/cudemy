a2f887485b332a3a54f3671a91b8c2e1b2e0c9eb
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-11-24 22:10:18 -0300

    format

--- packages/rabbitmq/src/setup.ts
+++ packages/rabbitmq/src/setup.ts
@@ -1,17 +1,91 @@
-import { logger } from '@/libs/logger/winston';
-import { getChannels } from '@/libs/rabbitmq';
-import { Exchange, Queue, RoutingKey } from '@/libs/rabbitmq/constants';
-import { MessageHandlerProps } from '@/libs/rabbitmq/types';
 import amqplib, { ConsumeMessage } from 'amqplib';
-import { EXCHANGES, QUEUE_BINDINGS, QUEUES } from './constants';
+import {
+  Exchange,
+  EXCHANGES,
+  Queue,
+  QUEUE_BINDINGS,
+  QUEUES,
+  RoutingKey,
+} from './constants';
 import { enrollmentToCoursesHandler } from './enrollment-to-courses.handler';
 import { mediaToCoursesHandler } from './media-to-courses.handler';
+import { MessageHandlerProps } from './types';
+
+// ---------------------------
+// VALUES
+// ---------------------------
 
 let isShuttingDown = false;
 let isReconnecting = false;
 let connection: amqplib.ChannelModel | undefined;
 let producerChannel: amqplib.ConfirmChannel | undefined;
 let consumerChannel: amqplib.Channel | undefined;
+
+// ---------------------------
+// CONNECTION
+// ---------------------------
+
+export const connectRabbitMQ = async (url: string) => {
+  connection = await amqplib.connect(url);
+  console.log('rabbitmq:connected');
+
+  connection.on('error', (err) => {
+    console.error(`rabbitmq:error ${String(err)}`);
+  });
+
+  connection.on('close', () => {
+    if (isShuttingDown || isReconnecting) return;
+
+    console.warn('rabbitmq:connection closed, reconnecting');
+
+    const reconnect = async () => {
+      isReconnecting = true;
+
+      while (isReconnecting) {
+        try {
+          // await setConnection();
+          isReconnecting = false;
+        } catch (err) {
+          console.error(`rabbitmq:error ${String(err)}`);
+          await new Promise((res) => setTimeout(res, 5000));
+        }
+      }
+    };
+    void reconnect();
+  });
+
+  try {
+    const connection = getConnection();
+
+    producerChannel = await connection.createConfirmChannel();
+    consumerChannel = await connection.createChannel();
+    console.log('rabbitmq:channels sets');
+
+    const bindEvents = (ch: amqplib.Channel) => {
+      ch.on('error', (err) => {
+        console.error(`rabbitmq:error ${String(err)}`);
+      });
+
+      ch.on('close', () => {
+        console.warn('rabbitmq:channel closed, closing connection');
+
+        const closeConnection = async () => {
+          try {
+            await connection.close();
+          } catch (err) {
+            console.error(`rabbitmq:error ${String(err)}`);
+          }
+        };
+        void closeConnection();
+      });
+    };
+    bindEvents(producerChannel);
+    bindEvents(consumerChannel);
+  } catch (err) {
+    console.error(`rabbitmq:error ${String(err)}`);
+    // await closeConnection();
+  }
+};
 
 export const getConnection = () => {
   if (!connection) throw new Error('RabbitMQ not connected');
@@ -25,101 +99,9 @@ export const getChannels = () => {
   return { producerChannel, consumerChannel };
 };
 
-export const connectRabbitMQ = async (url: string) => {
-  connection = await amqplib.connect(url);
-  logger.info('rabbitmq:connected');
-
-  connection.on('error', (err) => logger.error(`rabbitmq:error ${err}`));
-
-  connection.on('close', () => {
-    if (isShuttingDown || isReconnecting) return;
-
-    logger.warn('rabbitmq:connection closed, reconnecting');
-
-    const reconnect = async () => {
-      isReconnecting = true;
-
-      while (isReconnecting) {
-        try {
-          await setConnection();
-          isReconnecting = false;
-        } catch (err) {
-          logger.error(`rabbitmq:error ${err}`);
-          await new Promise((res) => setTimeout(res, 5000));
-        }
-      }
-    };
-    void reconnect();
-  });
-
-  try {
-    const connection = getConnection();
-
-    producerChannel = await connection.createConfirmChannel();
-    consumerChannel = await connection.createChannel();
-    logger.info('rabbitmq:channels sets');
-
-    const bindEvents = (ch: amqplib.Channel) => {
-      ch.on('error', (err) => logger.error(`rabbitmq:error ${err}`));
-
-      ch.on('close', () => {
-        logger.warn('rabbitmq:channel closed, closing connection');
-
-        const closeConnection = async () => {
-          try {
-            await connection.close();
-          } catch (err) {
-            logger.error(`rabbitmq:error ${err}`);
-          }
-        };
-        void closeConnection();
-      });
-    };
-    bindEvents(producerChannel);
-    bindEvents(consumerChannel);
-  } catch (err) {
-    logger.error(`rabbitmq:error ${err}`);
-    await closeConnection();
-  }
-};
-
-export const initRabbitMQ = async () => {
-  // Connection
-  await connectRabbitMQ();
-
-  // Exchanges
-  await assertProducerExchange(EXCHANGES.COURSES);
-
-  const consumerExchanges = [EXCHANGES.MEDIA, EXCHANGES.ENROLLMENT] as const;
-  for (const ex of consumerExchanges) {
-    await assertConsumerExchange(ex);
-  }
-
-  // Queues
-  const queueBindings = [
-    QUEUE_BINDINGS.MEDIA_TO_COURSES,
-    QUEUE_BINDINGS.ENROLLMENT_TO_COURSES,
-  ] as const;
-  for (const binding of queueBindings) {
-    await setupConsumerQueue(binding);
-  }
-
-  // Consumers
-  const consumers = [
-    [QUEUES.MEDIA_TO_COURSES, mediaToCoursesHandler],
-    [QUEUES.ENROLLMENT_TO_COURSES, enrollmentToCoursesHandler],
-  ] as const;
-  for (const [queue, handler] of consumers) {
-    await startConsumer(queue, handler);
-  }
-};
-
-export const shutdownRabbitMQ = async () => {
-  isShuttingDown = true;
-
-  if (connection) await connection.close();
-  connection = undefined;
-};
+// ---------------------------
+// SETUP
+// ---------------------------
 
 export const assertProducerExchange = async (exchange: Exchange) => {
   const { producerChannel } = getChannels();
@@ -172,7 +154,7 @@ export const startConsumer = async (
         });
         consumerChannel.ack(msg);
       } catch (err) {
-        logger.error(`rabbitmq:error ${err}`);
+        console.error(`rabbitmq:error ${String(err)}`);
         consumerChannel.nack(msg, false, false);
       }
     })();
@@ -180,4 +162,46 @@ export const startConsumer = async (
   await consumerChannel.consume(queue, onMessage, {
     noAck: false, // Messages require manual acknowledgment
   });
+};
+
+// ---------------------------
+// MAIN
+// ---------------------------
+
+export const initRabbitMQ = async () => {
+  // Connection
+  // await connectRabbitMQ();
+
+  // Exchanges
+  await assertProducerExchange(EXCHANGES.COURSES);
+
+  const consumerExchanges = [EXCHANGES.MEDIA, EXCHANGES.ENROLLMENT] as const;
+  for (const ex of consumerExchanges) {
+    await assertConsumerExchange(ex);
+  }
+
+  // Queues
+  const queueBindings = [
+    QUEUE_BINDINGS.MEDIA_TO_COURSES,
+    QUEUE_BINDINGS.ENROLLMENT_TO_COURSES,
+  ] as const;
+  for (const binding of queueBindings) {
+    await setupConsumerQueue(binding);
+  }
+
+  // Consumers
+  const consumers = [
+    [QUEUES.MEDIA_TO_COURSES, mediaToCoursesHandler],
+    [QUEUES.ENROLLMENT_TO_COURSES, enrollmentToCoursesHandler],
+  ] as const;
+  for (const [queue, handler] of consumers) {
+    await startConsumer(queue, handler);
+  }
+};
+
+export const shutdownRabbitMQ = async () => {
+  isShuttingDown = true;
+
+  if (connection) await connection.close();
+  connection = undefined;
 };
