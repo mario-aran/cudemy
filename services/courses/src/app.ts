94c7f704c78a2779412ab2e14b469bdeb296e8f8
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-12-20 12:17:14 -0300

    format

--- services/courses/src/app.ts
+++ services/courses/src/app.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import { morganMiddleware } from './libs/logger';
-import { routes } from './routes';
 
 export const app = express();
 
 app.use(morganMiddleware);
 app.use(express.json()); // Body parser
-app.use(routes); // Must be placed after req middlewares
