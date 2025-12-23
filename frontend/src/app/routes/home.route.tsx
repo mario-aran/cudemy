51b436e635640289a9c1220c34abd7796bb3db76
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-12-23 16:23:06 -0300

    format

---
+++ frontend/src/app/routes/home.route.tsx
@@ -0,0 +1,5 @@
+export const HomeRoute = () => (
+  <main>
+    <h1>HomeRoute</h1>
+  </main>
+);
---
+++ frontend/src/app/routes/not-found.route.tsx
@@ -0,0 +1,5 @@
+export const NotFoundRoute = () => (
+  <main>
+    <h1>NotfoundRoute</h1>
+  </main>
+);
