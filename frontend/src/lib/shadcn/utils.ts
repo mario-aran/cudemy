835e1077f186daf6415ca14588f33deddff02834
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-12-23 16:19:19 -0300

    shadcn utils

---
+++ frontend/src/lib/shadcn/utils.ts
@@ -0,0 +1,11 @@
+/* note:
+- DO NOT RENAME OR MOVE THIS FILE — used by components.json
+- File created by shadcn cli
+*/
+
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
