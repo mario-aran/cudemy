fbc0aa1588c574ea18b986bc524fbc630d5852f6
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-12-29 18:07:32 -0300

    format

--- frontend/web/src/app/router.tsx
+++ frontend/web/src/app/router.tsx
@@ -6,10 +6,11 @@ import { ProtectedLayout } from '@/components/layouts/protected-layout';
 import { PATHS } from '@/constants/paths';
 import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router/dom';
-import { CourseDetailRoute } from './routes/course/detail';
-import { CoursePlayerRoute } from './routes/course/player';
+import { CourseDetailRoute } from './routes/course/course-detail';
+import { CoursePlayerRoute } from './routes/course/course-player';
 import { HomeRoute } from './routes/home';
-import { InstructorUploadRoute } from './routes/instructor/upload';
+import { InstructorUploadRoute } from './routes/instructor/instructor-upload';
+import { LandingRoute } from './routes/landing';
 import { NotFoundRoute } from './routes/not-found';
 
 const router = createBrowserRouter([
@@ -19,15 +20,15 @@ const router = createBrowserRouter([
   },
   {
     Component: LandingLayout,
-    children: [{ index: true, Component: HomeRoute }],
+    children: [{ index: true, Component: LandingRoute }],
   },
   {
     Component: ProtectedLayout,
     children: [
       { index: true, Component: HomeRoute },
-      { path: PATHS.INSTRUCTOR_UPLOAD, Component: InstructorUploadRoute },
       { path: PATHS.COURSE_ID, Component: CourseDetailRoute },
       { path: PATHS.COURSE_ID_PLAYER, Component: CoursePlayerRoute },
+      { path: PATHS.INSTRUCTOR_UPLOAD, Component: InstructorUploadRoute },
     ],
   },
 ]);
