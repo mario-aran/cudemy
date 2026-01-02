2afd1bb43cbc534a2d359981f29f2f545506748b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2026-01-02 15:35:54 -0300

    provider

--- frontend/web/src/app/provider.tsx
+++ frontend/web/src/app/provider.tsx
@@ -1,12 +1,9 @@
-import { DarkModeProvider } from '@/lib/shadcn/dark-mode/provider';
+import { ShadcnProvider } from '@/lib/shadcn/shadcn-provider';
 import { TanstackQueryProvider } from '@/lib/tanstack/tanstack-query-provider';
 import type { PropsWithChildren } from 'react';
 
 export const AppProvider = ({ children }: PropsWithChildren) => (
   <TanstackQueryProvider>
-    <DarkModeProvider>
-      {/* Rest of your app */}
-      {children}
-    </DarkModeProvider>
+    <ShadcnProvider>{children}</ShadcnProvider>
   </TanstackQueryProvider>
 );
