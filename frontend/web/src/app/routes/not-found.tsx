842ef6ad12cfeb93bb4499ebf89a8e5ca51efb77
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2026-01-02 16:20:39 -0300

    format

--- frontend/web/src/app/routes/not-found.tsx
+++ frontend/web/src/app/routes/not-found.tsx
@@ -1,5 +1,22 @@
+import { Button } from '@/lib/shadcn/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import { Link } from 'react-router';
+
 export const NotFoundRoute = () => (
-  <>
-    <h1>Not Found Route</h1>
-  </>
+  <div className="w-full max-w-xs space-y-4 text-center">
+    <h1 className="text-4xl font-semibold">404</h1>
+
+    <h2 className="text-xl font-semibold">Page Not Found</h2>
+
+    <p className="text-muted-foreground">
+      Oops! The page you're looking for doesn't exist.
+    </p>
+
+    <Button asChild className="w-full">
+      <Link to="/">
+        <ArrowLeft className="mr-2 h-4 w-4" />
+        Back to Home
+      </Link>
+    </Button>
+  </div>
 );
