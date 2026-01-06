4e802cb21bccbff6809f7d16fadf75c0e38d5a9e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2026-01-06 16:37:27 -0300

    footer

---
+++ frontend/web/src/components/ui/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => (
+  <footer className="border-t">
+    <div className="container mx-auto border-x px-4 py-6 text-center text-sm">
+      <p>© {new Date().getFullYear()} Mario Arancibia. All rights reserved.</p>
+    </div>
+  </footer>
+);
