1d6f5642350d4039f46597365c29f9f9836b97f1
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2026-01-07 18:45:59 -0300

    settings-actions component

---
+++ frontend/web/src/components/ui/settings-actions.tsx
@@ -0,0 +1,7 @@
+import { ModeToggle } from '@/lib/shadcn/dark-mode/mode-toggle';
+
+export const SettingsActions = () => (
+  <div className="flex items-center gap-2">
+    <ModeToggle />
+  </div>
+);
